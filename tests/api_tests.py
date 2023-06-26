eae40b7b4b4abb87d51e10b8845b5a5650a1c3dd
David Wass <david.wass@bjss.com>
2023-06-26 10:03:06 +0000

    [ERSSUP-67731]-[]-[Upgrade test utils to 2.0]-[DMW]

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -1,82 +1,38 @@
-from functools import partial
-
 import pytest
-from aiohttp import ClientResponse
-from api_test_utils.api_session_client import APISessionClient
-from api_test_utils.api_test_session_config import APITestSessionConfig
-from api_test_utils import poll_until, env
+from pytest_check import check
+import requests
+from jsonpath_rw import parse
 
 
-async def _is_deployed(resp: ClientResponse, api_test_config: APITestSessionConfig) -> bool:
-
-    if resp.status != 200:
-        return False
-    body = await resp.json()
-    return body.get("commitId") == api_test_config.commit_id
-
-
-async def _is_deployed_and_connected(resp: ClientResponse, api_test_config: APITestSessionConfig) -> bool:
-
-    if resp.status != 200:
-        return False
-    body = await resp.json()
-    return body.get("commitId") == api_test_config.commit_id and body.get("status") == "pass"
-
-
-async def is_401(resp: ClientResponse) -> bool:
-    return resp.status == 401
-
-
-@pytest.mark.e2e
 @pytest.mark.smoketest
-def test_output_test_config(api_test_config: APITestSessionConfig):
-    print(api_test_config)
+class TestStatusEndpoints:
+    def test_ping_endpoint(self, service_url):
+        response = requests.get(f"{service_url}/_ping")
+        with check:
+            assert response.status_code == 200, (
+                f"UNEXPECTED RESPONSE: " f"Actual response status code = {response.status_code}"
+            )
 
-
-@pytest.mark.e2e
-@pytest.mark.smoketest
-@pytest.mark.asyncio
-async def test_wait_for_ping(api_client: APISessionClient, api_test_config: APITestSessionConfig):
-    """
-        test for _ping ..  this uses poll_until to wait until the correct SOURCE_COMMIT_ID ( from env var )
-        is available
-    """
-
-    is_deployed = partial(_is_deployed, api_test_config=api_test_config)
-
-    await poll_until(
-        make_request=lambda: api_client.get('_ping'),
-        until=is_deployed,
-        timeout=120
-    )
-
-
-@pytest.mark.e2e
-@pytest.mark.smoketest
-@pytest.mark.asyncio
-async def test_check_status_is_secured(api_client: APISessionClient):
-
-    await poll_until(
-        make_request=lambda: api_client.get('_status'),
-        until=is_401,
-        timeout=120
-    )
-
-
-@pytest.mark.e2e
-@pytest.mark.smoketest
-@pytest.mark.asyncio
-async def test_wait_for_status(api_client: APISessionClient, api_test_config: APITestSessionConfig):
-
-    """
-        test for _status ..  this uses poll_until to wait until the correct SOURCE_COMMIT_ID ( from env var )
-        is available
-    """
-
-    is_deployed = partial(_is_deployed_and_connected, api_test_config=api_test_config)
-
-    await poll_until(
-        make_request=lambda: api_client.get('_status', headers={'apikey': env.status_endpoint_api_key()}),
-        until=is_deployed,
-        timeout=120
-    )
+    def test_status_endpoint(self, service_url, status_endpoint_api_key):
+        response = requests.get(
+            f"{service_url}/_status", headers={"apikey": status_endpoint_api_key}
+        )
+        with check:
+            assert response.status_code == 200, (
+                "UNEXPECTED RESPONSE: "
+                f"Actual response status code = {response.status_code}"
+            )
+        with check:
+            expression = parse("$.status")
+            matches = [match.value for match in expression.find(response.json())]
+            assert matches.count("pass") == 1, (
+                "UNEXPECTED RESPONSE: "
+                "Health check failed: $.status != 'pass'"
+            )
+        with check:
+            expression = parse('$.checks["healthcheckService:status"][*].status')
+            matches = [match.value for match in expression.find(response.json())]
+            assert matches.count("pass") == len(matches), (
+                "UNEXPECTED RESPONSE: "
+                "Health check failed: $.checks['healthcheckService:status'][*].status)] != 'pass'"
+            )
